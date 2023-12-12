f7ef44882b011dd1b18982c36a091e1602010af2
RogerHerrera98 <roger.20070021@itsmotul.edu.mx>
2023-12-11 22:02:37 -0600

    Commit

--- resources/views/Perros/Perfil.blade.php
+++ resources/views/Perros/Perfil.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,63 +15,97 @@
     @vite('resources/css/app.css')
     <title>Perfil</title>
 </head>
+
 <body>
-<nav class="bg-[#E3EEFF] h-20 w-full flex flex-row justify-between items-center">
-    <div class="flex justify-between items-center m-1">
-        <h1 style="font-family: 'Moo Lah Lah', sans-serif;" class="text-black md:p-5 text-3xl">P&M</h1>
-        <img class="w-14 h-14" src="Recursos/Logo2.png" alt="Icono P&M">
+    <nav class="bg-[#E3EEFF] h-20 w-full flex flex-row justify-between items-center">
+        <div class="flex justify-between items-center m-1">
+            <h1 style="font-family: 'Moo Lah Lah', sans-serif;" class="text-black md:p-5 text-3xl">P&M</h1>
+            <img class="w-14 h-14" src="Recursos/Logo2.png" alt="Icono P&M">
+        </div>
+        <div class="flex flex-col justify-center items-center m-1">
+            <img src="Recursos/Carnet.png" alt="carnet" class="w-14 h-14">
+            <p style="font-family: 'Vibur', cursive;" class="text-sm text-black text-center ">Carnet</p>
+        </div>
+    </nav>
+    <div class="bg-[#956e37] w-full h-16 flex justify-center items-center ">
+        <h1 style="font-family: 'Vibur', cursive;" class="text-4xl text-white">Perfil del Perro</h1>
     </div>
-    <div class="flex flex-col justify-center items-center m-1">
-        <img src="Recursos/Carnet.png" alt="carnet" class="w-14 h-14">
-        <p style="font-family: 'Vibur', cursive;" class="text-sm text-black text-center ">Carnet</p>
-    </div>
-</nav>
-<div class="bg-[#956e37] w-full h-16 flex justify-center items-center ">
-
-            <h1 style="font-family: 'Vibur', cursive;" class="text-4xl text-white">Perfil del Perro</h1>
-
-</div>
-<div class="h-screen  w-full bg-no-repeat bg-cover bg-center flex flex-col justify-center items-center" style="background-image: url('Recursos/Fondo_Inicio.jpg');">
-    <div class=" w-4/5 h-full  bg-[#956e37]/50 my-2">
-        <div>
-            <div>
-                <img src="Recursos/Foto_Perro.jpg" alt="">
-            </div>
-            <form action="" method="post">
-            <div class="flex flex-col w-2/3 mb-4">
-                    <label for="nombre"  style="font-family: 'Wendy One', sans-serif" class="text-[#781e08] text-lg">Nombres</label>
-                    <input type="text" name="nombre" id="nombre"  style="font-family: 'Wendy One', sans-serif" class="rounded-full bg-[#e0ae65] px-4 h-8 text-md" oninput="validarNumero()">
-                    <p id="mensaje" class="text-red-500 text-sm mt-2"></p>
+    <div class="h-screen  w-full bg-no-repeat bg-cover bg-center flex flex-col justify-center items-center"
+        style="background-image: url('Recursos/Fondo_Inicio.jpg');">
+        <div class=" w-4/5 h-full  bg-[#956e37]/50 my-4">
+            <div class="flex flex-row m-10 w-full h-full">
+                <div class="mr-4 w-1/2">
+                    <img src="Recursos/Foto_Perro.jpg" alt="" class="w-full">
                 </div>
-                <div class="flex flex-col w-2/3 mb-4">
-                    <label for="raza" style="font-family: 'Wendy One', sans-serif" class="text-[#781e08] text-lg">Raza</label>
-                    <input type="text" name="raza" id="raza" style="font-family: 'Wendy One', sans-serif" class="rounded-full bg-[#e0ae65] px-4 h-8 text-md" oninput="validarNumero2()">
-                    <p id="mensaje2" class="text-red-500 text-sm mt-2"></p>
-                </div>
-                <div>
-                    <label for="sexo">Sexo</label>
+                <form action="" method="post" class="ml-4 w-1/2">
+                    <div class="flex flex-col w-1/2 mb-4">
+                        <label for="nombre" style="font-family: 'Wendy One', sans-serif"
+                            class="text-[#781e08] text-lg">Nombres</label>
+                        <input type="text" name="nombre" id="nombre" style="font-family: 'Wendy One', sans-serif"
+                            class="rounded-full bg-[#e0ae65] px-4 h-8 text-md" oninput="validarNumero()">
+                        <p id="mensaje" class="text-red-500 text-sm mt-2"></p>
+                    </div>
+
+                    <div class="flex flex-col w-1/2 mb-4">
+                        <label for="raza" style="font-family: 'Wendy One', sans-serif"
+                            class="text-[#781e08] text-lg">Raza</label>
+                        <input type="text" name="raza" id="raza" style="font-family: 'Wendy One', sans-serif"
+                            class="rounded-full bg-[#e0ae65] px-4 h-8 text-md" oninput="validarNumero2()">
+                        <p id="mensaje2" class="text-red-500 text-sm mt-2"></p>
+                    </div>
                     <div>
-                        <input type="checkbox" name="checkHembra" id="checkHembra">
-                        <label for="raza" style="font-family: 'Wendy One', sans-serif" class="text-[#781e08] text-lg">Hembra</label>
+                        <label for="dateNacimiento" style="font-family: 'Wendy One', sans-serif"
+                            class="text-[#781e08] text-lg">Sexo</label>
+                        <div class="flex flex-row justify-between space-x-4 w-1/2">
+                            <div class="flex flex-row ">
+                            <input type="checkbox" name="checkHembra" id="checkHembra" class="w-5 h-5">
+                            <label for="checkHembra" style="font-family: 'Wendy One', sans-serif"
+                                class="text-[#781e08] text-lg">Hembra</label>
+                            </div>
+                           <div class="flex flex-row justify-center items-center">
+                            <input type="checkbox" name="checkMacho" id="checkMacho"  class="w-5 h-5">
+                            <label for="checkMacho" style="font-family: 'Wendy One', sans-serif"
+                                class="text-[#781e08] text-lg">Macho</label>
+
+                           </div>
+                        </div>
+
+                    </div>
+                    <div>
+                        <label for="dateNacimiento" style="font-family: 'Wendy One', sans-serif"
+                            class="text-[#781e08] text-lg">Fecha de nacimiento</label>
+                        <input type="date" name="dateNacmiento" id="dateNacmiento">
+
+                    </div>
+                    <div>
+                        <label for="dateNacimiento" style="font-family: 'Wendy One', sans-serif"
+                            class="text-[#781e08] text-lg">Discapacidad</label>
+                        <div>
+                            <input type="checkbox" name="checkSi" id="checkSi">
+                            <label for="checkSi" style="font-family: 'Wendy One', sans-serif"
+                                class="text-[#781e08] text-lg">Si</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="checkNo" id="checkNo">
+                            <label for="checkNo" style="font-family: 'Wendy One', sans-serif"
+                                class="text-[#781e08] text-lg">No</label>
+                        </div>
+                    </div>
+                    <div>
+                        <button type="button">Editar</button>
+                        <img src="Recursos/Boton_Hueso.png" alt="">
 
                     </div>
 
-                </div>
-                <div>
-
-                </div>
-                <div>
-
-                </div>
-                
 
 
-            </form>
+                </form>
+            </div>
         </div>
-    </div>
 
-</div>
+    </div>
 
 
 </body>
+
 </html>
